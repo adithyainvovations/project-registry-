2abd0ff185a839fa9af6ef559e7b9383806b82db
adithyainvovations <adithyan551@gmail.com>
2026-03-26 21:25:01 +0530

    Delete project-registry directory

--- project-registry/download_model.py
+++
@@ -1,4 +0,0 @@
-from sentence_transformers import SentenceTransformer
-print("Downloading and caching all-MiniLM-L6-v2...")
-model = SentenceTransformer('all-MiniLM-L6-v2')
-print("Model loaded successfully!")
